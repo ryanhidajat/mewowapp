60f5b926e6e85560ff1ac2fb8076415aa6a500c9
Ryan Hidajat <insomni4k@yahoo.com>
2015-12-28 17:35:19 +0700

    onsen
    update with onsen framework

--- www/js/script.js
+++ www/js/script.js
@@ -1,5 +1,50 @@
 $(document).on("pageinit", ".ui-page", function () {
 	console.log('any page loaded');
+jQuery(function($) {
+
+    $.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=1&count=10')
+        .success(function(response) {
+			
+		i = 0;
+		while (i < 10) {
+			
+			$("#page1").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
+
+			i++;
+			}
+        });
+});
+
+jQuery(function($) {
+
+	$.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=2&count=10')
+		.success(function(response) {
+			
+		i = 0;
+		while (i < 10) {
+			
+			$("#page2").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
+
+			i++;
+			}
+		});
+	});
+	
+
+jQuery(function($) {
+
+	$.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=3&count=10')
+		.success(function(response) {
+			
+		i = 0;
+		while (i < 10) {
+			
+			$("#page3").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
+
+			i++;
+			}
+		});
+	});	
 });
 
 $( document ).delegate("#home-page", "pageinit", function() {    
@@ -11,47 +56,5 @@ $( document ).delegate("#home-page", "pageinit", function() {
 //beforeSend: function() { $.mobile.showPageLoadingMsg(); }, //Show spinner
 //complete: function() { $.mobile.hidePageLoadingMsg() }, //Hide spinner
 
-jQuery(function($) {
 
-    $.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=1')
-        .success(function(response) {
-			
-		i = 0;
-		while (i < 30) {
-			
-			$("#image").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
 
-			i++;
-			}
-        });
-});
-
-jQuery(function($) {
-
-    $.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=2')
-        .success(function(response) {
-			
-		i = 0;
-		while (i < 30) {
-			
-			$("#image").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
-
-			i++;
-			}
-        });
-});
-
-jQuery(function($) {
-
-    $.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=3')
-        .success(function(response) {
-			
-		i = 0;
-		while (i < 30) {
-			
-			$("#image").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
-
-			i++;
-			}
-        });
-});
