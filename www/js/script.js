b6886917c4b1fea2a851a36cdf798096df1cfc31
Ryan Hidajat <insomni4k@yahoo.com>
2015-12-28 14:28:59 +0700

    index update

--- www/js/script.js
+++ www/js/script.js
@@ -8,21 +8,12 @@ $( document ).delegate("#home-page", "pageinit", function() {
 
 });
 
-function refreshPage()
-{
-    jQuery.mobile.changePage(window.location.href, {
-        allowSamePageTransition: true,
-        transition: 'flip',
-        reloadPage: true
-    });
-}
-
 //beforeSend: function() { $.mobile.showPageLoadingMsg(); }, //Show spinner
 //complete: function() { $.mobile.hidePageLoadingMsg() }, //Hide spinner
 
 jQuery(function($) {
 
-    $.getJSON('http://mewow.dewitech.com/shop/?json=get_recent_posts&callback=?')
+    $.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=1')
         .success(function(response) {
 			
 		i = 0;
@@ -35,3 +26,32 @@ jQuery(function($) {
         });
 });
 
+jQuery(function($) {
+
+    $.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=2')
+        .success(function(response) {
+			
+		i = 0;
+		while (i < 30) {
+			
+			$("#image").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
+
+			i++;
+			}
+        });
+});
+
+jQuery(function($) {
+
+    $.getJSON('http://mewow.dewitech.com/?json=get_recent_posts&callback=?&post_type=product&page=3')
+        .success(function(response) {
+			
+		i = 0;
+		while (i < 30) {
+			
+			$("#image").append("<li class='ui-grid-a'><img src='" + response.posts[i].thumbnail + "' /><br /><a href='" + response.posts[i].slug + "' >" + response.posts[i].title + "</a><br>" + response.posts[i].content + "</li>");
+
+			i++;
+			}
+        });
+});
